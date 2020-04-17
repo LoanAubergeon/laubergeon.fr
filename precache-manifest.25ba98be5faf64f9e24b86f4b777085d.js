dc32764b2cd84ba2f9947da40b9abd2ce88a0e3e
Loan <laubergeon@gmail.com>
2020-04-17 12:27:26 +0200

    Updates

---
+++ precache-manifest.25ba98be5faf64f9e24b86f4b777085d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "393b3f8a42054b986decb7c84432a220",
+    "url": "/laubergeon.fr/index.html"
+  },
+  {
+    "revision": "d688330874ce6a4d6f8a",
+    "url": "/laubergeon.fr/static/css/2.160cff76.chunk.css"
+  },
+  {
+    "revision": "c3ef86bbe85bdc6f8402",
+    "url": "/laubergeon.fr/static/css/main.f5fd5c6e.chunk.css"
+  },
+  {
+    "revision": "d688330874ce6a4d6f8a",
+    "url": "/laubergeon.fr/static/js/2.40dd23fc.chunk.js"
+  },
+  {
+    "revision": "249567eb41b7ce6a613bf00c4a366397",
+    "url": "/laubergeon.fr/static/js/2.40dd23fc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c3ef86bbe85bdc6f8402",
+    "url": "/laubergeon.fr/static/js/main.b3c8e50b.chunk.js"
+  },
+  {
+    "revision": "33f1e7fe990e564445ef",
+    "url": "/laubergeon.fr/static/js/runtime-main.682a973c.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.45409061c645e893ab0e6108f2c215ee.js"
+  "/laubergeon.fr/precache-manifest.25ba98be5faf64f9e24b86f4b777085d.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/laubergeon.fr/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
