1bf28c0b23b98779b0c9d39a47902d4a7ec58870
Loan <laubergeon@gmail.com>
2020-05-08 12:41:39 +0200

    Updates

---
+++ precache-manifest.e13add128ece071a0ac74f3f1f396ff7.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50a48138e2a27dcca5fa3c81857a47b8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4c73ad5a1788ca92b64f",
+    "url": "/static/css/2.1174ccc1.chunk.css"
+  },
+  {
+    "revision": "62f866d73d76d8746a12",
+    "url": "/static/css/main.e881cd1c.chunk.css"
+  },
+  {
+    "revision": "4c73ad5a1788ca92b64f",
+    "url": "/static/js/2.4c76a050.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.4c76a050.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "62f866d73d76d8746a12",
+    "url": "/static/js/main.e629714d.chunk.js"
+  },
+  {
+    "revision": "bb317cc66b446ff55592",
+    "url": "/static/js/runtime-main.1e368351.js"
+  },
+  {
+    "revision": "b684341a250e71567f88f62128986bbb",
+    "url": "/static/media/Citelio.b684341a.png"
+  },
+  {
+    "revision": "67bec294472afc3acdf01ad78244859d",
+    "url": "/static/media/GetALift.67bec294.png"
+  },
+  {
+    "revision": "573220ffa6e56756682e9ab707c0aef0",
+    "url": "/static/media/PP2.573220ff.jpg"
+  },
+  {
+    "revision": "33f5ce9e5013ec212492179d5bb91912",
+    "url": "/static/media/bg3.33f5ce9e.jpg"
+  },
+  {
+    "revision": "f47e0492339c0dfdcb6125ac813aa38f",
+    "url": "/static/media/logo_thales.f47e0492.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/laubergeon.fr/precache-manifest.935ac93fe556bb2b2afa5722050fcadc.js"
+  "/precache-manifest.e13add128ece071a0ac74f3f1f396ff7.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/laubergeon.fr/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
